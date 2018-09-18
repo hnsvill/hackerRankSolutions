6302597803e95e0c4a0bac58b462513d309ba90c
Hannah Scovill <hnsvill@gmail.com>
2018-09-18 02:15:04 -0700

    Learned about deque

---
+++ arrayLeftRotation.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+#Arrays: Left Rotation
+#https://www.hackerrank.com/challenges/ctci-array-left-rotation/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
+
+import math
+import os
+import random
+import re
+import sys
+import collections
+
+# Complete the rotLeft function below.
+def rotLeft(a, d):
+    rotator = collections.deque(a)
+    rotator.rotate(0-abs(d)) #Make d negative in order to rotate left
+    return list(rotator)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nd = input().split()
+
+    n = int(nd[0])
+
+    d = int(nd[1])
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = rotLeft(a, d)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
